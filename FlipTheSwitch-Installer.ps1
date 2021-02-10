daa186051a91deab1018802e8babcefdd10d7b1d
Thomas G <thimas.goetz@gmail.com>
2021-02-09 19:37:25 -0800

    Add files via upload
    I was able to get the script to make a hotkey and was able to put it all together in 1 package that walks you through setup

---
+++ FlipTheSwitch-Installer.ps1
@@ -0,0 +1,34 @@
+﻿  (Get-AudioDevice -list)
+  ($2 = Read-Host "Find the playback devices you wish to use. Enter the Index number of one of them now.")
+  ($1 = Read-Host "Put the second index number in now")
+ ## New-Item -Path "$Env:userprofile\Documents" -Name "FlipTheSwitch" -ItemType "directory"
+  
+    $DOCDIR = "$Env:userprofile\Documents"
+    $TARGETDIR = "$DOCDIR\FlipTheSwitch"
+    if(!(Test-Path -Path $TARGETDIR )){New-Item -ItemType directory -Path $TARGETDIR}
+    
+    if(Test-Path -Path "$Env:userprofile\Documents\FlipTheSwitch\FlipTheSwitch.ps1"){Remove-item -path "$Env:userprofile\Documents\FlipTheSwitch\FlipTheSwitch.ps1"}
+
+
+
+  New-Item -Path "$Env:userprofile\Documents\FlipTheSwitch\" -Name "FlipTheSwitch.txt" -Force -ItemType "file" -Value '##$V = Get-audiodevice -playback'
+  $File = "$Env:userprofile\Documents\FlipTheSwitch\FlipTheSwitch.txt"
+  Add-content $File " ##Powershell requires this spacer for some reason"
+  Add-content $File "if ((Get-audiodevice -playback).Index -eq $2)"
+  Add-content $File "{set-audiodevice -index $1}"
+  Add-content $File "else"
+  Add-content $File "{set-audiodevice -index $2}"
+  Rename-Item -Path "$Env:userprofile\Documents\FlipTheSwitch\FlipTheSwitch.txt" -Newname "FlipTheSwitch.ps1"
+
+if (Test-Path -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk")
+{Remove-Item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk"}
+
+$TargetFile = """C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"""
+$ShortcutFile = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk"
+$Hotkey = Read-Host "The hotkey will be Ctrl+Alt+(Letter). Type Desired Hotkey Letter"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
+$Shortcut.Arguments = "-version 4 -ExecutionPolicy Bypass -File $env:USERPROFILE\Documents\FlipTheSwitch\FlipTheSwitch.ps1"
+$Shortcut.TargetPath = "$TargetFile"
+$Shortcut.hotkey = "Ctrl+Alt+$Hotkey"
+$Shortcut.Save()
--- Hotkey.ps1
+++ Hotkey.ps1
@@ -1,9 +1,12 @@
+if (Test-Path -Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk")
+{Remove-Item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk"}
+
 $TargetFile = """C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"""
-$ShortcutFile = "C:\Users\$env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk"
+$ShortcutFile = "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\FlipTheSwitch.lnk"
 $Hotkey = Read-Host "The hotkey will be Ctrl+Alt+(Letter). Type Desired Hotkey Letter"
 $WScriptShell = New-Object -ComObject WScript.Shell
 $Shortcut = $WScriptShell.CreateShortcut($ShortcutFile)
-$Shortcut.Arguments = "-version 4 -ExecutionPolicy Bypass -File C:\Admin\Scripts\FlipTheSwitch\FlipTheSwitch-live.ps1"
-$Shortcut.TargetPath = $TargetFile
+$Shortcut.Arguments = "-version 4 -ExecutionPolicy Bypass -File $env:USERPROFILE\Documents\FlipTheSwitch\FlipTheSwitch.ps1"
+$Shortcut.TargetPath = "$TargetFile"
 $Shortcut.hotkey = "Ctrl+Alt+$Hotkey"
 $Shortcut.Save()
